893624fa5daba9c4ee86df0ce5c2ec74b6be4387
Msizi Gumede <u24784606@tuks.co.za>
2025-03-07 11:49:29 +0200

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MsiziGumede2StudyGroup
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MsiziGumede2StudyGroup.MvcApplication" Language="C#" %>
