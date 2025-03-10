29a998afdc9a1d52c74f64e0d9950b0b7de0a0e6
Msizi Gumede <u24784606@tuks.co.za>
2025-03-10 17:59:34 +0200

    Msizi

---
+++ Controllers/myStudentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MsiziGumede2StudyGroup.Controllers
+{
+    public class myStudentsController : Controller
+    {
+        // GET: myStudents
+        public ActionResult StudentsView()
+        {
+            List<Models.myStudents> students = new List<Models.myStudents>();
+
+            students.Add(new Models.myStudents { StudentNumber = "u24784606", Name = "Msizi", Surname = "Gumede", Email = "u24784606@tuks.co.za", myLink =("~/HTML/Msizi.html") });
+            students.Add(new Models.myStudents { StudentNumber = "u24634400", Name = "Boitshoko", Surname = "Mphahlele", Email = "u24634400@tuks.co.za", myLink = "~/HTML/Boitshoko.html" });
+            students.Add(new Models.myStudents { StudentNumber = "u24717739", Name = "Ritshidze", Surname = "Nedzimauli", Email = "u24717739@tuks.co.za", myLink = "~/HTML/Ritshidze1.html" });
+            students.Add(new Models.myStudents { StudentNumber = "u24924718", Name = "Gundo", Surname = "Ndou", Email = "u24924718@tuks.co.za", myLink = "~/HTML/Gundo.html" });
+            students.Add(new Models.myStudents { StudentNumber = "u24768929", Name = "Lethabo", Surname = "Moses", Email = "u24768929@tuks.co.za", myLink = "~/HTML/Lethabo.html" });
+
+
+            return View(students);
+        }
+    }
+}
---
+++ Models/myStudents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MsiziGumede2StudyGroup.Models
+{
+	public class myStudents
+	{
+        [Display(Name = "StudentNumber")]
+
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")]
+
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+
+        public string Surname { get; set; }
+
+        [Display(Name = "Email")]
+
+        public string Email { get; set; }
+
+        [Display(Name = "Link to Personal Page")]
+        public string myLink { get; set; } 
+    }
+}
